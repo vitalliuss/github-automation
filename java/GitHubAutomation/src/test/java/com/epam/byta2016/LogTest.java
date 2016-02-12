5334213b73f97effe035025d4380866d7e7586b9
Vitali Shulha <Vitali_Shulha@epam.com>
2016-02-12 15:20:31 +0300

    add new tests + screenshot and highlight samples

--- java/GitHubAutomation/src/test/java/com/epam/byta2016/LogTest.java
+++ java/GitHubAutomation/src/test/java/com/epam/byta2016/LogTest.java
@@ -1,15 +1,22 @@
 package com.epam.byta2016;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Vitali_Shulha on 12-Feb-16.
  */
 public class LogTest {
 
-    @Test
+    @Test(enabled = true)
     public void oneCanWriteMessagesToLog(){
         Logger logger = LogManager.getRootLogger();
 
@@ -19,5 +26,56 @@ public class LogTest {
         logger.fatal("Fatal message");
         logger.warn("Warn message");
         logger.trace("Trace message");
+    }
+
+
+    @Test
+    public void oneCanFail(){
+        Assert.fail();
+    }
+
+    @Test(enabled = false)
+    public void oneCanTakeScreenshotWithWebDriver(){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.reddit.com/");
+
+        WebElement buttonLogin = driver.findElement(By.xpath("//button[text()='login']"));
+        highlightElement(driver, buttonLogin);
+    }
+
+    public String takeScreenshot(WebDriver driver) {
+        String path;
+        try {
+            File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            path = "./target/screenshots/" + source.getName();
+            FileUtils.copyFile(source, new File(path));
+        }
+        catch(IOException e) {
+            path = "Failed to capture screenshot: " + e.getMessage();
+        }
+        return path;
+    }
+
+    private void highlightElement(WebDriver driver, WebElement element)
+    {
+        String bg = element.getCssValue("backgroundColor");
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("arguments[0].style.backgroundColor = '" + "yellow" + "'", element);
+        takeScreenshot(driver);
+        sleep(2000); // just for demo
+        js.executeScript("arguments[0].style.backgroundColor = '" + bg + "'", element);
+    }
+
+    public void sleep(int millis)
+    {
+        try
+        {
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
