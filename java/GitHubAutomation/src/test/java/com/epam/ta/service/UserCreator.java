532069abb89cd2e15ac04332e5da8aa86c6fbfbe
Vitali Shulha <vitali_shulha@epam.com>
2019-03-05 18:49:26 +0300

    remove configs

--- java/GitHubAutomation/src/test/java/com/epam/ta/service/UserCreator.java
+++ java/GitHubAutomation/src/test/java/com/epam/ta/service/UserCreator.java
@@ -4,19 +4,19 @@ import com.epam.ta.model.User;
 
 public class UserCreator {
 
-    public static final String TESTDATA_USER_NAME = "testdata.user.name";
-    public static final String TESTDATA_USER_PASSWORD = "testdata.user.password";
+    public static final String USER_NAME = "test-automation-user";
+    public static final String USER_PASSWORD = "Time4Death!";
 
     public static User withCredentialsFromProperty(){
-        return new User(TestDataReader.getTestData(TESTDATA_USER_NAME),
-                TestDataReader.getTestData(TESTDATA_USER_PASSWORD));
+        return new User(USER_NAME,
+                USER_PASSWORD);
     }
 
     public static User withEmptyUsername(){
-        return new User("", TestDataReader.getTestData(TESTDATA_USER_PASSWORD));
+        return new User("", USER_PASSWORD);
     }
 
     public static User withEmptyPassword(){
-        return new User(TestDataReader.getTestData(TESTDATA_USER_NAME), "");
+        return new User(USER_NAME, "");
     }
 }
