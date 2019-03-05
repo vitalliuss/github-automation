cbfa4eb014d25bd87310b328cea712a155c9519e
Vitali Shulha <vitali_shulha@epam.com>
2019-03-05 19:38:59 +0300

    remove webdrivermanager

--- java/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ java/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -1,12 +1,12 @@
 package com.epam.ta.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
 
+    private static final String RESOURCES_PATH = "src\\test\\resources\\";
     private static WebDriver driver;
 
 
@@ -16,11 +16,11 @@ public class DriverSingleton {
         if (null == driver){
             switch (System.getProperty("browser")){
                 case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
+                    System.setProperty("webdriver.gecko.driver", RESOURCES_PATH + "geckodriver.exe");
                     driver = new FirefoxDriver();
                 }
                 default: {
-                    WebDriverManager.chromedriver().setup();
+                    System.setProperty("webdriver.chrome.driver", RESOURCES_PATH + "chromedriver.exe");
                     driver = new ChromeDriver();
                 }
             }
