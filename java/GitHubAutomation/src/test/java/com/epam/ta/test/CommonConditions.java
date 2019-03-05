d670702700b2ae41a17d9336e42544a57c968b51
Vitali Shulha <vitali_shulha@epam.com>
2019-03-05 19:43:44 +0300

    remove driver singletone

--- java/GitHubAutomation/src/test/java/com/epam/ta/test/CommonConditions.java
+++ java/GitHubAutomation/src/test/java/com/epam/ta/test/CommonConditions.java
@@ -1,27 +1,29 @@
 package com.epam.ta.test;
 
-import com.epam.ta.driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
 
 public class CommonConditions {
 
     protected WebDriver driver;
     protected static final String USER_NAME = "test-automation-user";
     protected static final String USER_PASSWORD = "Time4Death!";
+    private static final String RESOURCES_PATH = "src\\test\\resources\\";
 
 
     @BeforeMethod()
     public void setUp()
     {
-        driver = DriverSingleton.getDriver();
+        System.setProperty("webdriver.chrome.driver", RESOURCES_PATH + "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @AfterMethod(alwaysRun = true)
     public void stopBrowser()
     {
-        DriverSingleton.closeDriver();
+        driver.quit();
     }
 }
