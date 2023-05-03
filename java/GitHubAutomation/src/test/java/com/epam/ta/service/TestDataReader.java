25b1e26e5b9a10dc7441a8edeff32041f9e268e1
AlexHeckendorn <alex.heckendorn@gmail.com>
2023-05-02 20:37:40 -0600

    Adding and making changes to existing framework

--- java/GitHubAutomation/src/test/java/com/epam/ta/service/TestDataReader.java
+++ java/GitHubAutomation/src/test/java/com/epam/ta/service/TestDataReader.java
@@ -11,10 +11,17 @@ public class TestDataReader {
         this.environment = environment;
     }
 
+    private static ResourceBundle resourceBundle;
+
     public static void setEnvironment(String environment) {
         resourceBundle = ResourceBundle.getBundle(environment);
     }
-    private static ResourceBundle resourceBundle;
+
+
+    //Remove comment below & also comment out lines 14 - 18 in order to execute tests via command line in older version
+    //private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
+
+
 
     public static String getTestData(String key){
         return resourceBundle.getString(key);
